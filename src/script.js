bebf4bf02a7411f46ab0407b2b8bb1baa066c457
yejineee <yygeniee@gmail.com>
2022-03-18 00:49:12 +0900

    Feat: heart geometry 추가

--- src/script.js
+++ src/script.js
@@ -12,16 +12,27 @@ const canvas = document.querySelector('#webgl')
 const scene = new THREE.Scene()
 
 // Geometry
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const heartShape = new THREE.Shape();
 
-// Materials
+heartShape.moveTo( 25, 25 );
+heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
+heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
+heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const extrudeSettings = { depth: 20, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 2, bevelThickness: 0.5 };
+const geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
+
+// Material
+const material = new THREE.MeshBasicMaterial({color: 0xFF1493, wireframe: true})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const heartMesh = new THREE.Mesh( geometry, material);
+heartMesh.geometry.rotateX(Math.PI)
+heartMesh.geometry.translate(-25, 100, 0)
+scene.add(heartMesh)
 
 // Lights
 const light = new THREE.PointLight(0xffffff, 0.2, 100);
@@ -44,12 +55,12 @@ const sizes = {
 
 const aspectRatio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(60, aspectRatio, 1, 2000);
-camera.position.z = 5;
+camera.position.set(0,150,200);
 camera.lookAt(new THREE.Vector3(0,0,0));  
 scene.add(camera);
 
 /**
- * Rendererㄴ
+ * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -64,9 +75,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const tick = () =>
 {
     // TODO : 회전하게 만들기
-
-    // Update Orbital Controls
-    // controls.update()
 
     // Render
     renderer.render(scene, camera)
