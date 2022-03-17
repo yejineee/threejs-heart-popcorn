55075c20c67e253cce01356e40aa31350810b3bd
yejineee <yygeniee@gmail.com>
2022-03-17 22:11:00 +0900

    Init

--- src/script.js
+++ src/script.js
@@ -1,18 +1,17 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
 // Debug
 const gui = new dat.GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('#webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
+// Geometry
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
 // Materials
@@ -25,12 +24,7 @@ const sphere = new THREE.Mesh(geometry,material)
 scene.add(sphere)
 
 // Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// TODO : Light 추가하기
 
 /**
  * Sizes
@@ -40,37 +34,16 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+// TODO : 반응형으로 만들기
 
 /**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+// TODO 카메라 추가하기
 
 /**
- * Renderer
+ * Rendererㄴ
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -82,15 +55,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    // TODO : 회전하게 만들기
 
     // Update Orbital Controls
     // controls.update()
